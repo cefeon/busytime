7c715e77712d0ae2d3a77d8c4546c3e19af6ee39
cefeon <cefeon@outlook.com>
2021-05-31 16:42:00 +0200

    Reversed tasks list order

--- src/main/java/com/cefeon/busytime/Task.java
+++ src/main/java/com/cefeon/busytime/Task.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -49,6 +50,7 @@ public class Task {
             List<Task> tasks = new ArrayList<>();
             lines.forEach(x -> tasks.add(new Task(x.split(" ", 2)[1], x.split(" ", 2)[0])));
             setTasksDuration(tasks);
+            Collections.reverse(tasks);
             return tasks;
         }
     }
